9adf798c1c3ab62e8c647db894206ca4fde7fdbe
voidfunctionreturn0 <voidfunctionreturn0@gmail.com>
2024-11-14 14:27:17 +0900

    ExchangeRateFetcher를 24시간마다 업데이트하는 로직 삭제

--- lib/kimper/exchange_rate_fetcher.ex
+++ lib/kimper/exchange_rate_fetcher.ex
@@ -2,7 +2,6 @@ defmodule Kimper.ExchangeRateFetcher do
   use GenServer
 
   @initial_state %{usd_krw_rate: nil}
-  @update_interval  24 * 60 * 60 * 1000 #24시간
   @exchange_rate_api_url "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"  # 참고: https://github.com/fawazahmed0/exchange-api
 
   def start_link(_) do
@@ -24,14 +23,10 @@ defmodule Kimper.ExchangeRateFetcher do
 
   def handle_info(:fetch_exchange_rate, state) do
     new_exchange_rate = fetch_usd_krw_rate()
-    schedule_fetch()
     Kimper.Storage.set_exchange_rate(new_exchange_rate)
     {:noreply, %{state | usd_krw_rate: new_exchange_rate}}
   end
 
-  defp schedule_fetch() do
-    Process.send_after(self(), :fetch_exchange_rate, @update_interval)
-  end
 
   defp fetch_usd_krw_rate() do
     response = HTTPoison.get!(@exchange_rate_api_url).body |> Jason.decode!()
