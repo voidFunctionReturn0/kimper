5f67ae68c1482ff69a02e3d95a0ca6d6c7aaf94d
voidfunctionreturn0 <voidfunctionreturn0@gmail.com>
2024-11-16 22:25:43 +0900

    BybitPriceFetcher 리턴형식 잘못된 것 수정

--- lib/kimper/bybit_price_fetcher.ex
+++ lib/kimper/bybit_price_fetcher.ex
@@ -1,11 +1,11 @@
 defmodule Kimper.BybitPriceFetcher do
    # TODO1: 20초마다 ping을 보내면 네트워크 이슈 피할 수 있다고 함 https://bybit-exchange.github.io/docs/v5/ws/connect#how-to-send-the-heartbeat-packet
-
    use WebSockex
+   require Logger
    alias Kimper.Storage
 
   @url "wss://stream.bybit.com/v5/public/spot"
-  @heart_beat_interval 20_000 # 20초
+  @heartbeat_interval 20_000 # 20초
 
   def start_link(state) do
     {:ok, pid} = WebSockex.start_link(@url, __MODULE__, state)
@@ -18,9 +18,42 @@ defmodule Kimper.BybitPriceFetcher do
     })
 
     WebSockex.send_frame(pid, {:text, subscription_message})
-    # TODO3: Process.send_after(self(), :ping, @heart_beat_interval) -> [error] Supervisor received unexpected message: :ping
-
     {:ok, pid}
+  end
+
+  def init(state) do
+    schedule_heartbeat()
+    {:ok, state}
+  end
+
+  defp schedule_heartbeat do
+    Process.send_after(self(), :send_heartbeat, @heartbeat_interval)
+  end
+
+  def handle_cast(:send_heartbeat, state) do
+    case send_heartbeat() do
+      :ok                -> Logger.info("## Heartbeat sent successfully")
+      {:error, reason}   -> Logger.error("## Failed to send Heartbeat: #{inspect(reason)}")
+    end
+
+    schedule_heartbeat()
+
+    {:ok, state}
+  end
+
+  def handle_cast({:error, reason}, state) do
+    Logger.error("## Error in WebSocket: #{inspect(reason)}")
+    {:noreply, state}
+  end
+
+  defp send_heartbeat() do
+    heartbeat_message = Jason.encode!(%{"op" => "ping"})
+    WebSockex.send_frame(self(), {:text, heartbeat_message})
+  end
+
+  def handle_frame({:text, %{"success" => true, "ret_msg" => "pong"}}, state) do
+    Logger.info("## Received pong")
+    {:ok, state}
   end
 
   def handle_frame({:text, message}, state) do
@@ -36,11 +69,8 @@ defmodule Kimper.BybitPriceFetcher do
     {:ok, state}
   end
 
-  def handle_info(:ping, pid) do
-    IO.inspect(pid, label: "## ping")
-    heart_beat_message = Jason.encode!(%{"op" => "ping"})
-    WebSockex.send_frame(pid, {:text, heart_beat_message})
-    Process.send_after(self(), :ping, @heart_beat_interval)
-    {:ok, pid}
+  def handle_disconnect(reason, state) do
+    Logger.error("## Disconnected from WebSocket: #{inspect(reason)}")
+    {:reconnect, state}
   end
 end
